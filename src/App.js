63275406793832178353ed8f4964dc0c526f3ce7
m9 <paulbravomacedo@gmail.com>
2023-12-04 19:28:12 -0500

    Initial Text

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Proyecto Base del Frontend SentimentAI
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/paulbm9"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go to Repositories
         </a>
       </header>
     </div>
