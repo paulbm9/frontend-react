cf1084d46b263ebe598a7629ffc1b84a30ac3f48
m9 <paulbravomacedo@gmail.com>
2023-12-04 21:46:54 -0500

    mod: index base

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Probando
+          Proyecto Base en ReactJS - SentimentAI
         </p>
         <a
           className="App-link"
